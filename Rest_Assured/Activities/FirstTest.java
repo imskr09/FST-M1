68ae504d896606c8bd16fc5ee98721c18daa8547
Sajan kumar rajak <46090748+imskr09@users.noreply.github.com>
2025-09-15 13:11:22 +0530

    Add files via upload

---
+++ Rest_Assured/Activities/FirstTest.java
@@ -0,0 +1,82 @@
+package examples;
+
+import static org.testng.Assert.assertEquals;
+
+import org.hamcrest.Matchers;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class FirstTest {
+	
+	
+	// GET https://petstore.swagger.io/v2/pet/findByStatus?status=sold
+	
+	
+	@Test
+	public void getRequestQueryParam() {
+		// This method will send a GET request to the Petstore API with a query parameter
+		// Example: GET https://petstore.swagger.io/v2/pet/findByStatus?status=sold
+		
+		Response response = 
+				RestAssured.given().
+				  baseUri("https://petstore.swagger.io/v2/pet").
+				  header("Content-Type", "application/json"). //Define Request Type
+				  header("Accept", "application/json"). //Define Response Type
+				  queryParam("status", "sold").
+				when().
+				  get("/findByStatus");
+		
+		// Print the response body
+		System.out.println("Response Body: " + response.getBody().asString());
+		System.out.println("==============================================");
+		System.out.println("Response Body: " + response.getBody().asPrettyString());
+		//System.out.println("Response: " + response.getHeaders.asList());
+		
+		String petStatus = response.then().extract().path("[0]['status']");
+		System.out.println("Pet Status: " + petStatus);
+		
+		//Assertions
+		Assert.assertEquals(petStatus, "sold");
+		
+		//RestAssured assertions
+		response.then().statusCode(200).
+		body("[0]['status']", Matchers.equalTo("sold"));
+		
+	} 
+	
+	// GET https://petstore.swagger.io/v2/pet/{petId}
+	
+	public void getRequestWithPathParam() {
+		// This method will send a GET request to the Petstore API with a path parameter
+		// Example: GET https://petstore.swagger.io/v2/pet/{petId}
+		
+		
+				RestAssured.given().
+				  baseUri("https://petstore.swagger.io/v2/pet").
+				  header("Content-Type", "application/json"). //Define Request Type
+				  header("Accept", "application/json"). //Define Response Type
+				  pathParam("petId", 12).
+				when().
+				  get("/{petId}").
+				then().
+				   statusCode(200).
+				   body("status", Matchers.equalTo("available")).
+				   body("name", Matchers.equalTo("doggie"));
+				   
+		
+		/*// Print the response body
+		System.out.println("Response Body: " + response.getBody().asString());
+		
+		String petName = response.then().extract().path("name");
+		System.out.println("Pet Name: " + petName);
+		
+		//Assertions
+		Assert.assertEquals(petName, "doggie");*/
+		
+	}
+	
+
+}
---
+++ Rest_Assured/Activities/GitHubProjectTest.java
@@ -0,0 +1,90 @@
+package project;
+
+import java.util.HashMap;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class GitHubProjectTest {
+	
+	
+	String sshKey ="";// "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFIbrLb6/S3L0oUr3TF8emiBoryKfooSvYY6JgkRqVbV";
+	
+	int keyId;
+	RequestSpecification requestSpec;
+	ResponseSpecification responseSpec;
+	
+	@BeforeClass
+	public void setUp()
+	
+	{
+		//Initialize the request specification
+		requestSpec=new RequestSpecBuilder()
+				.setBaseUri( "https://api.github.com/user/keys")				
+				.addHeader("Content-Type", "application/json")
+				.addHeader("Authorization","token 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")
+				.addHeader("X-GitHub-Api-Version", "2022-11-28")
+				.build();
+		
+		
+		//Initialize the response specification
+		responseSpec= new ResponseSpecBuilder()
+				.expectBody("title",Matchers.equalTo("TestKey"))
+				.expectResponseTime(Matchers.lessThanOrEqualTo(3000L))
+				.expectStatusCode(200)
+				.expectContentType("application/json")
+				.build();
+	}
+	
+	
+	@Test(priority=1)
+	public void postRequestTest()
+	{
+		//Request body as Hashmap
+		HashMap<String, String> reqBody = new HashMap<>();
+		reqBody.put("title", "TestKey");
+		reqBody.put("key", sshKey);
+		
+		// send request, Save response
+		Response response = RestAssured.given().
+				spec(requestSpec).
+				body(reqBody).log().all().
+				when().post();
+		
+		//Extract the id from the response body
+		keyId = response.then().extract().path("id");
+		
+		//Assertions
+		response.then().statusCode(201).spec(responseSpec).log().all();
+		
+	}
+		
+
+		@Test(priority=2)
+		public void getRequestTest()
+		{
+			//send request, receive response and assert
+			RestAssured.given().spec(requestSpec).pathParam("keyId", keyId).
+			when().get("/{keyId}").
+			then().statusCode(200).spec(responseSpec);
+		}
+		@Test(priority=3)
+		public void deleteRequestTest()
+		{
+			//send request, receive response and assert
+			RestAssured.given().spec(requestSpec).pathParam("keyId", keyId).
+			when().get("/{keyId}").
+			then().statusCode(204);
+		}
+		
+	}
+
+
